aae55b4dad11a824441a3703423443b020165b53
yassinaghan-afk <yassinaghan@gmail.com>
2026-07-12 02:57:24 +0700

    Revert "fix(reviews): force-dynamic homepage so deactivate is not one render behind"
    This reverts commit e4e66e9f39e892aaca4d4b08ea6cd979c8f0de09.

--- app/page.tsx
+++ app/page.tsx
@@ -46,8 +46,7 @@ const FAQ = dynamic(
   { loading: () => null }
 );
 
-export const dynamic = "force-dynamic";
-export const revalidate = 0;
+export const revalidate = 3600;
 
 export default async function Home() {
   const [homepage, instagramShowcase] = await Promise.all([
